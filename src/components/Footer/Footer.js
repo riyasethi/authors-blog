6431f69b2bb9928163e82c572ec59d84f398ce1f
riyasethi <riyasethi721@gmail.com>
2022-10-17 13:27:33 +0530

    Authors Blog

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,9 @@
-import React, {memo} from 'react'
+import React, { memo } from 'react'
 
 function Footer() {
   return (
     <div className="card-footer text-muted text-center font-weight-bolder p-5 m-2">
-      Made with &hearts; by Mohit Sojitra
+      Made with &hearts; by Riya Sethi
     </div>
   )
 }
